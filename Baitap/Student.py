6b1b25c68ffb6f41e8c9864d1fb5d20b021dec9a
henryford-cyber <namphan.021101@gmail.com>
2023-05-16 23:46:04 +0700

    add project

--- Baitap/Student.py
+++
@@ -1,9 +0,0 @@
-class Student:
-  def __init__(self, id, name, birtDate,mathScores,physicScore,chemistryScore):
-    self.id = id
-    self.name = name
-    self.birtDate = birtDate
-    self.mathScores = mathScores
-    self.physicScore = physicScore
-    self.chemistryScore = chemistryScore
-   
